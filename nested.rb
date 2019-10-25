9ce2169200f735941ea93011586cbf5ae6b280ce
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-10-25 06:55:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,6 +73,10 @@ def adding_matz
 # }
 
 	programmer_hash = 
+	
+	
+	
+	
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -88,8 +92,7 @@ def adding_matz
         }
     
  		}
-      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
- 		  programmer_hash[:yukihiro_matsumoto][:languages] = '"LISP",  "C"'
+      
 
     
 end
