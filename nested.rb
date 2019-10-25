f6b8242de70183adb4d2e584ae0eb5eb51608d12
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-10-25 06:42:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -13,9 +13,9 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
-          return programmer_hash[:grace_hopper]
+          
     }
-  
+  return programmer_hash[:grace_hopper]
   
 end
 
